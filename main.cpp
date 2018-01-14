7363744f23854310b519c806a16f79a96cf36dab
Andreas Chrysopoulos <a.chrisopoulos@gmail.com>
2018-01-14 15:56:35 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  oop_project
+//
+//  Created by Andreas Chrysopoulos on 14/01/2018.
+//  Copyright © 2018 Andreas Chrysopoulos & Giannis Mystakidis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
