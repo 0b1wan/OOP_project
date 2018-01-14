27e0d95ccd69ff400279bddd86dcfb8e0b5a94c1
Andreas Chrysopoulos <a.chrisopoulos@gmail.com>
2018-01-14 16:03:06 +0200

    revert

--- hello.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  hello.cpp
-//  oop_project
-//
-//  Created by Andreas Chrysopoulos on 14/01/2018.
-//  Copyright © 2018 Andreas Chrysopoulos & Giannis Mystakidis. All rights reserved.
-//
-
-#include "hello.hpp"
