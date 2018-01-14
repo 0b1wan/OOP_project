cadbdcd18a1ab3d1f513ceca884556937e675c82
Andreas Chrysopoulos <a.chrisopoulos@gmail.com>
2018-01-14 16:01:59 +0200

    hello.cpp

---
+++ hello.hpp
@@ -0,0 +1,14 @@
+//
+//  hello.hpp
+//  oop_project
+//
+//  Created by Andreas Chrysopoulos on 14/01/2018.
+//  Copyright © 2018 Andreas Chrysopoulos & Giannis Mystakidis. All rights reserved.
+//
+
+#ifndef hello_hpp
+#define hello_hpp
+
+#include <stdio.h>
+
+#endif /* hello_hpp */
